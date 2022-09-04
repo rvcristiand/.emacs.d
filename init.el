c9db1fc870c8f5f96213a6209c3baeb750c77a38
rvcristiand <rvcristiand@unal.edu.co>
2022-09-03 22:47:24 -0500

    migrate to desktop

--- init.el
+++ init.el
@@ -6,9 +6,12 @@
 ;; Enables basic packaging support
 (require 'package)
 
+
 ;; Adds the Melpa archive to the list of available repositories
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 ;; Initializes the package infrastructure
 (package-initialize)
@@ -23,7 +26,9 @@
 (defvar myPackages
   '(better-defaults                 ;; Set up some better Emacs defaults
     elpy                            ;; Emacs Lisp Python Environment
-    material-theme                  ;; Theme
+    material-theme                  ;; material Theme
+    dracula-theme                   ;; dracula theme
+    openwith
     )
   )
 
@@ -37,17 +42,56 @@
 ;; ===================================
 ;; Basic Customization
 ;; ===================================
+(setq inhibit-startup-message t)               ;; Hide the startup message
+;; (load-theme 'material t)                    ;; Load material theme
+(load-theme 'dracula t)                        ;; Load dracula theme
+(global-linum-mode t)                          ;; Enable line numbers globally
+(global-unset-key (kbd "C-z"))                 ;; Disable c-z
+(set-face-attribute 'default nil :height 180)  ;; font size
+(global-visual-line-mode 1)                    ;; visual line mode
+(delete-selection-mode 1)                      ;; delect mark selection
+(tool-bar-mode -1)                             ;; disable tool bar
+(menu-bar-mode -1)                             ;; disable the `menu bar`
+(openwith-mode 1)                              ;; openwith
+(global-auto-complete-mode t)                  ;; auto-complete-mode
 
-(setq inhibit-startup-message t)    ;; Hide the startup message
-(load-theme 'material t)            ;; Load material theme
-(global-linum-mode t)               ;; Enable line numbers globally
-(global-unset-key (kbd "C-z"))      ;; Disable c-z
+(setq openwith-associations
+      (list
+       ;; (list (openwith-make-extension-regexp
+       ;;        '("mpg" "mpeg" "mp3" "mp4"
+       ;;          "avi" "wmv" "wav" "mov" "flv"
+       ;;          "ogm" "ogg" "mkv"))
+       ;;       "vlc"
+       ;;       '(file))
+       ;; (list (openwith-make-extension-regexp
+       ;;        '("xbm" "pbm" "pgm" "ppm" "pnm"
+       ;;          "png" "gif" "bmp" "tif" "jpeg" "jpg"))
+       ;;       "geeqie"
+       ;;       '(file))
+       ;; (list (openwith-make-extension-regexp
+       ;;        '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
+       ;;       "libreoffice"
+       ;;       '(file))
+       ;; '("\\.lyx" "lyx" (file))
+       ;; '("\\.chm" "kchmviewer" (file))
+       (list (openwith-make-extension-regexp
+              '("pdf" "ps" "ps.gz" "dvi"))
+             "evince"
+             '(file))
+       ))
+
+;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;; ====================================
 ;; Development Setup
 ;; ====================================
 ;; Enable elpy
-;; (elpy-enable)
+(elpy-enable)
+;; (setq elpy-rpc-virtualenv-path 'global)
 
 ;; My terminal stuff
 ;; (defvar counter 0)
@@ -64,20 +108,20 @@
 ;;   (switch-to-buffer buf-title)
 ;; )
 
-;; multi-term
-(add-to-list 'load-path "~/.emacs.d/lisp/")
-(require 'multi-term)
+;; ;; multi-term
+;; (add-to-list 'load-path "~/.emacs.d/lisp/")
+;; (require 'multi-term)
 
-(autoload 'multi-term "multi-term" nil t)
-(autoload 'multi-term-next "multi-term" nil t)
+;; (autoload 'multi-term "multi-term" nil t)
+;; (autoload 'multi-term-next "multi-term" nil t)
 
-(setq multi-term-program "/bin/zsh")
+;; (setq multi-term-program "/bin/zsh")
 
-(global-set-key (kbd "C-c t") 'multi-term-next)
-(global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
+;; (global-set-key (kbd "C-c t") 'multi-term-next)
+;; (global-set-key (kbd "C-c T") 'multi-term) ;; create a new one
 
 ;; clipboard
-(setq x-select-enable-clipboard t)
+;; (setq x-select-enable-clipboard t)
 
 ;; User-Defined init.el ends here
 (custom-set-variables
@@ -85,7 +129,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(material-theme better-defaults)))
+ '(package-selected-packages
+   '(markdown-mode auto-complete typescript-mode tide magit openwith material-theme better-defaults)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
